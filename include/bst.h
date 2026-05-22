383ab66ebe773c851b716369968e4255d6ac7c88
RaweNist <116841332+RaweNist@users.noreply.github.com>
2026-05-22 11:39:33 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,72 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-
+template <typename T>
+class BST {
+ private:
+  struct Node {
+      T data;
+      int count;
+      Node* left;
+      Node* right;
+  };
+  Node* root;
+  Node* addNode(Node* root, T data) {
+    if (root == nullptr) {
+      root = new Node;
+      root->data = data;
+      root->count = 1;
+      root->left = nullptr;
+      root->right = nullptr;
+    }
+    else if (root->data > data)
+      root->left = addNode(root->left, data);
+    else if (root->data < data)
+      root->right = addNode(root->right, data);
+    else root->count++;
+    return root;
+  }
+  int searchTree(Node* root, T data) {
+    if (!root) return 0;
+    else if (data > root->data)
+      return searchTree(root->right, data);
+    else if (data < root->data)
+      return searchTree(root->left, data);
+    else return root->count;
+  }
+  int countHeight(Node* root) {
+    if (root == nullptr) return 0;
+    int a = countHeight(root->left);
+    int b = countHeight(root->right);
+    if (a > b)
+      return a + 1;
+    else return b + 1;
+  }
+  void deleteTree(Node* node) {
+    if (node) {
+      deleteTree(node->left);
+      deleteTree(node->right);
+      delete node;
+    }
+  }
+ public:
+  BST() : root(nullptr) {}
+  ~BST() {
+    deleteTree(root);
+  }
+  void add(const T& data) {
+    root = addNode(root, data);
+  }
+  int search(T data) {
+    return searchTree(root, data);
+  }
+  int depth() {
+    return countHeight(root) - 1;
+  }
+  int countNodes(Node* root) {
+    if (root == nullptr)
+      return 0;
+    else return countNodes(root->left) + countNodes(root->right) + 1;
+  }
+};
 #endif  // INCLUDE_BST_H_
